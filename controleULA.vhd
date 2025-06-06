ca85b71ca163cab1b3dd470907b5f84d3511d109
Gabriel Castro <164261705+gabrielfcastro@users.noreply.github.com>
2025-06-05 23:10:28 -0300

    Update controleULA.vhd

--- controleULA.vhd
+++ controleULA.vhd
@@ -7,10 +7,10 @@ use work.riscv_pkg.all;
 entity ULAControle is
     port 
 	 (
-			ALUop: 			in std_logic_vector(2 downto 0);
-			funct3: 			in std_logic_vector(2 downto 0);
-			funct7: 			in std_logic;											-- Pegando só o bit mais importate
-			controleULA: 	out std_logic_vector(3 downto 0)
+		ALUop: 			in std_logic_vector(2 downto 0);
+		funct3: 			in std_logic_vector(2 downto 0);
+		funct7: 			in std_logic;					-- Pegando só o bit mais importate
+		controleULA: 	out std_logic_vector(3 downto 0)
 	);
 end ULAControle;
 
@@ -20,33 +20,33 @@ begin
 	process (ALUop, funct3, funct7)
 	begin
 	case ALUop is
-		when "000" => 																	-- lw e sw (fazem add)
+		when "000" => 									-- lw e sw (fazem add)
 			controleULA <= "0010";
 				
-		when "001" => 																	-- beq
+		when "001" => 									-- beq
 			controleULA <= "0110";
 				
-		when "010" => 																	-- instruções do tipo-R (add, sub, and, or, slt)
+		when "010" => 									-- instruções do tipo-R (add, sub, and, or, slt)
 			case funct3 is
 				when "000" => 
 					if funct7 = '0' then 
-						controleULA <= "0010"; 										-- add
+						controleULA <= "0010"; 				-- add
 					else 
-						controleULA <= "0110"; 										-- sub
+						controleULA <= "0110"; 				-- sub
 					end if;
  						
-				when "111" => 															-- and
+				when "111" => 							-- and
 					controleULA <= "0000";
 					
 				when "110" => 
-					controleULA <= "0001"; 											-- or
+					controleULA <= "0001"; 					-- or
 						
 				when "010" =>
-					controleULA <= "0111"; 											-- slt
+					controleULA <= "0111"; 					-- slt
 				end case;
 						
 			when "011" =>					
-				controleULA <= "0010"; 												-- addi e jalr (os dois fazem add). Note que o jal não passa pela ULA
+				controleULA <= "0010"; 						-- addi e jalr (os dois fazem add). Note que o jal não passa pela ULA
 							
 	end case;
 	end process;
